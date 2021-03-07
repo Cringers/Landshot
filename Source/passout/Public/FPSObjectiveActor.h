99e4d85accf63c70e120b096e6f690f565af3636
Ethan DeSon <ethandeson@gmail.com>
2021-03-07 12:09:41 -0800

    testing branches

--- Source/passout/Public/FPSObjectiveActor.h
+++ Source/passout/Public/FPSObjectiveActor.h
@@ -28,6 +28,8 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void PlayEffects();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
