978d42016f95262f56f7ce4effdc1dd16529a1b1
Adam V <5581459+avere001@users.noreply.github.com>
2021-04-30 20:33:33 -0700

    Fix crosshair math

--- Source/passout/Private/UI/passoutHUD.cpp
+++ Source/passout/Private/UI/passoutHUD.cpp
@@ -25,8 +25,8 @@ void ApassoutHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition( (Center.X - CrosshairTex->GetSizeX() / 2),
+										   (Center.Y - CrosshairTex->GetSizeY() / 2));
 
 	// draw the crosshair
 	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
